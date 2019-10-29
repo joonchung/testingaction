f01f0189f91ae7f88e1ba004dfdcfeb1177caf4f
joonchung <55109818+joonchung@users.noreply.github.com>
2019-10-29 14:53:27 -0400

    Update main.tf

--- testing/main.tf
+++ testing/main.tf
@@ -1,1 +1,26 @@
+provider "google" {
+  region  = "northamerica-northeast1"
+}
 
+resource "google_monitoring_uptime_check_config" "http" {
+  display_name = "http-uptime-check"
+  timeout = "60s"
+  project = "firestore-test-3"
+  http_check {
+    path = "/some-path"
+    port = "8010"
+  }
+
+  monitored_resource {
+
+    type = "uptime_url"
+    labels = {
+      project_id = "firestore-test-3"
+      host = "192.168.1.1"
+    }
+  }
+
+  content_matchers {
+    content = example
+  }
+}
