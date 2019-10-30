236822575f5ffd761b5d8273f6cce74a50b948ac
joonchung <55109818+joonchung@users.noreply.github.com>
2019-10-30 16:26:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "${dasta.aws_ami.ubuntu.id}"
   instance_type = "tsf1.smalasdasddl"
 
   tags = {
