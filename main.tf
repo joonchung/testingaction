29ab8d02d54ff2699e3ba739ead24be6210fd5d0
joonchung <55109818+joonchung@users.noreply.github.com>
2019-10-31 15:09:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${dasfssfta.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micrsfso"
 
   tags = {
     Name = "HelloWorld"
