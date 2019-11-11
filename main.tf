10a0d93c6d540193e478cff61971eb840c50b737
Joon Chung <T947207@corp.ads>
2019-11-11 16:02:05 -0500

    test

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           - "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micrvvo"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelsfosfsWorld"
