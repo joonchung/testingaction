c5c78ea7a743a40680f3fdca8e5ca4dea4b12a4c
Joon Chung <T947207@corp.ads>
2019-11-12 14:06:20 -0500

    testing

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1"
 }
 
 data "aws_ami" "ubuntu" {
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.midfcro"
+  instance_type = "t2.midcro"
 
   tags = {
     Name = "HelsfosssffsfsWorld"
