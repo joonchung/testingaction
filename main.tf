e642f2fcc07e03b9d68f9e3212ed365a4fc295bf
joonchung <55109818+joonchung@users.noreply.github.com>
2019-12-05 13:17:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.misfdcro"
+  ami      = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micsdf"
 
   tags = {
-    Name = "HelsfosssffsfsWorld"
+    Name = "Hello World"
   }
 }
